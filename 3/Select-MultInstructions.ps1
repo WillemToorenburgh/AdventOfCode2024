e07be1c254a2a9ef69394dbd6b2a0bbbaaa3dd30
Willem Toorenburgh <willemtoorenburgh@gmail.com>
2024-12-16 15:21:45 -0800

    Part 2 complete! I evade writing a tokenizer for another day

--- 3/Select-MultInstructions.ps1
+++ 3/Select-MultInstructions.ps1
@@ -2,22 +2,52 @@ $ExampleString = @'
 xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
 '@
 
-# $InputString = $ExampleString
+$ExampleString2 = @'
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+'@
+
+# $InputString = $ExampleString2
 $InputString = Get-Content $PSScriptRoot/input.txt
 
 # Could try my hand at making a tokenizer, but first let's just regex the thing
 # If I really want to write a tokenizer, having something to compare it against is wise
 [Regex]$Pattern = "mul\((?<instruction>\d{1,3},\d{1,3})\)"
 
-$Result = Select-String -Pattern $Pattern -InputObject $InputString -CaseSensitive -AllMatches
+# $Result = Select-String -Pattern $Pattern -InputObject $InputString -CaseSensitive -AllMatches
+
+# $FinalSum = 0
+
+# ForEach ($Instruction in $Result.Matches) {
+#     # Instructions are always in group 1
+#     $Pair = $Instruction.Groups[1] -Split ','
+#     $FinalSum += [Int]$Pair[0] * [Int]$Pair[1]
+# }
+
+# Write-Host $Result
+# Write-Host "Sum of mult results is $FinalSum"
+
+
+### Part 2: no tokenizer necessary just get good at regex lol
+
+$Pattern2 = [Regex]"(do\(\))|(don't\(\))|(mul\((?<command>\d{1,3},\d{1,3})\))"
+$Selections = Select-String -List -AllMatches -CaseSensitive -InputObject $InputString -Pattern $Pattern2
 
 $FinalSum = 0
+$ShouldExecute = $True
 
-ForEach ($Instruction in $Result.Matches) {
-    # Instructions are always in group 1
-    $Pair = $Instruction.Groups[1] -Split ','
-    $FinalSum += [Int]$Pair[0] * [Int]$Pair[1]
+ForEach ($Command in $Selections.Matches) {
+    switch ($Command.Value) {
+        { $_.StartsWith("do(") } { $ShouldExecute = $True; Break }
+        { $_.StartsWith("don't(") } { $ShouldExecute = $False; Break }
+        { $_.StartsWith("mul(") } {
+            If (-Not $ShouldExecute) { Break }
+            $Pair = $Command.Groups[4] -Split ','
+            $FinalSum += [Int]$Pair[0] * [Int]$Pair[1]
+            Break
+        }
+        Default { Write-Warning "Not sure how to handle $Command"; Break }
+    }
 }
 
-Write-Host $Result
 Write-Host "Sum of mult results is $FinalSum"
+
